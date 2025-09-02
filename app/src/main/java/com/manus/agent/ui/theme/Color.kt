0937990ef88e5100023741ff745d50775b890bc2
‪Ahmad Kujume‬‏ <ahmadalmlk807@gmail.com>
2025-09-03 00:18:02 +0300

    Update Color.kt

--- app/src/main/java/com/manus/agent/ui/theme/Color.kt
+++ app/src/main/java/com/manus/agent/ui/theme/Color.kt
@@ -1,10 +1,20 @@
-دpackage com.manus.agent.ui.theme
+package com.manus.agent.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// الألوان الأصلية
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200  = Color(0xFF03DAC5)
 val Teal700  = Color(0xFF018786)
 val White    = Color(0xFFFFFFFF)
 val Black    = Color(0xFF000000)
+
+// ألوان مطابقة لما هو مستخدم في Theme.kt
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
