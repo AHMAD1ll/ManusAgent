d4c39405a983002afc2b63cd9435a193db99d309
‪Ahmad Kujume‬‏ <ahmadalmlk807@gmail.com>
2025-09-02 17:53:57 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,8 @@
-// *** تم تحديث هذا الملف بالكامل لضمان التوافق التام ***
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-    // *** هذا هو الجزء الذي سيحل المشكلة ***
-    // نحن نجبر المشروع على استخدام إصدارات محددة من الإضافات
-    plugins {
-        id("com.android.application") version "8.4.1" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     }
 }
 dependencyResolutionManagement {
