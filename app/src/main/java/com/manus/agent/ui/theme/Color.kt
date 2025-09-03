d7d9dc42dc8622a97e4d26f0f70cd64f26d98754
‪Ahmad Kujume‬‏ <ahmadalmlk807@gmail.com>
2025-09-03 03:40:47 +0300

    Update Color.kt

--- app/src/main/java/com/manus/agent/ui/theme/Color.kt
+++ app/src/main/java/com/manus/agent/ui/theme/Color.kt
@@ -2,19 +2,16 @@ package com.manus.agent.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// الألوان الأصلية
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200  = Color(0xFF03DAC5)
-val Teal700  = Color(0xFF018786)
-val White    = Color(0xFFFFFFFF)
-val Black    = Color(0xFF000000)
+val md_theme_light_primary = Color(0xFF6750A4)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF625B71)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFFFBFE)
+val md_theme_light_onBackground = Color(0xFF1C1B1F)
 
-// ألوان مطابقة لما هو مستخدم في Theme.kt
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_dark_primary = Color(0xFFD0BCFF)
+val md_theme_dark_onPrimary = Color(0xFF381E72)
+val md_theme_dark_secondary = Color(0xFFCCC2DC)
+val md_theme_dark_onSecondary = Color(0xFF332D41)
+val md_theme_dark_background = Color(0xFF1C1B1F)
+val md_theme_dark_onBackground = Color(0xFFE6E1E5)
